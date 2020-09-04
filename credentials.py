dc35ef2f87824f410b712f617151d283464d005f
Vector254 <ochrist7@gmail.com>
2020-09-04 11:56:02 +0300

    initial commit

---
+++ credentials.py
@@ -0,0 +1,9 @@
+class Credentials:
+    """Class that generates new instances of credentials"""
+
+    def __init__(self,accout,username,password):
+        """define properties for our objects."""
+
+        self.account= accout
+        self.username = username
+        self.password = password
---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    """Class that generates new instances of users"""
+
+    def __init__(self,username,password):
+        """define properties for our objects."""
+
+        self.username = username
+        self.password = password
+
